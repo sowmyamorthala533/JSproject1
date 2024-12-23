ae873095527df775c8c0694a626564006f60cec3
sowmyamorthala533 <morthalasowmya77@gmail.com>
2024-12-23 13:58:22 +0530

    kjhiu

---
+++ main.js
@@ -0,0 +1,97 @@
+"use strict";
+const SearchEL=document.getElementById("search")
+const formEL=document.getElementById("form")
+const MoviesContainer=document.querySelector(".movies-details")
+const PaginationsEL=document.querySelectorAll('.paginations ul li')
+const TvShowsContainer=document.querySelector(".Tv-details")
+//cbe386797cdd903b2235cf394e67f722
+//https://image.tmdb.org/t/p/w1280
+//https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=cbe386797cdd903b2235cf394e67f722
+const MoviesAPI="https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=cbe386797cdd903b2235cf394e67f722&page=1";
+const imagePath="https://image.tmdb.org/t/p/w1280";
+const searchUrl='https://api.themoviedb.org/3/search/movie?api_key=cbe386797cdd903b2235cf394e67f722&query="';
+const TvShowsAPI=
+"https://api.themoviedb.org/3/discover/tv?sort_by=popularity.desc&api_key=cbe386797cdd903b2235cf394e67f722&page=1";
+const searchTvShows='https://api.themoviedb.org/3/search/tv?api_key=cbe386797cdd903b2235cf394e67f722&query="';
+
+getMovies(MoviesAPI);
+
+
+//get movie function
+async function getMovies(url){
+    try{
+        const result=await fetch(url);
+        const data=await result.json()
+        showMovies(data.results);
+    }catch(error){
+    }
+}
+//display movies inside Html
+ function showMovies(movies){
+  MoviesContainer.innerHTML="";
+  movies.forEach((movie)=>{
+    const{title,poster_path,vote_average,release_date}=movie
+   const MoviesDisplay=document.createElement("div")
+ MoviesDisplay.classList.add("movies");
+ MoviesDisplay.innerHTML=` <img src="${imagePath+poster_path}" alt="">
+ <p class="movie-title">${title}</p>
+ <div class="short-des">
+    <p class="year">Date : ${release_date}</p>
+    <p class="rating">Vote : ${vote_average}</p>
+ </div>` ;
+ MoviesContainer.appendChild(MoviesDisplay);
+ });
+ }
+ //search element
+ formEL.addEventListener("submit",(e)=>{
+    e.preventDefault();
+    const  searchTerm=SearchEL.value;
+    if(searchTerm && searchTerm!==""){
+        getMovies(searchUrl+searchTerm);
+        getTvShows(searchTvShows+searchTerm);
+        SearchEL.value=""
+    }
+   else{
+    window.location.reload();
+   }
+ })
+//paginations
+ PaginationsEL.forEach((pages,index)=>{
+    pages.addEventListener("click",()=>{
+        if(getMovies){
+          getMovies(MoviesAPI+index)
+          
+        }
+        getTvShows(TvShowsAPI+index)
+    })
+ })
+
+ //tvShows
+ getTvShows(TvShowsAPI);
+
+//get movie function
+async function getTvShows(url){
+    try{
+        const result=await fetch(url);
+        const data=await result.json()
+        showTvShows(data.results);
+        console.log(result)
+    }catch(error){
+    }
+}
+function showTvShows (tvShows){
+  TvShowsContainer.innerHTML= "";
+  tvShows.forEach((tvShow)=>{
+    const {name,poster_path,vote_average,first_air_date}=tvShow;
+    const tvShowsDisplay=document.createElement("div");
+    tvShowsDisplay.classList.add("tvShows");
+    tvShowsDisplay.innerHTML=`<img src="${imagePath + poster_path}" alt="">
+      <p class="movie-title">${name}</p>
+      <div class="short-des">
+        <p class="year">Date: ${first_air_date || "N/A"}</p>
+        <p class="rating">Vote: ${vote_average || "N/A"}</p>
+      </div>`;
+      TvShowsContainer.appendChild(tvShowsDisplay);
+  
+  })
+}
--- script.js
+++ script.js
@@ -1,54 +1,145 @@
-var input = document.getElementById("textbox");
-      console.log(input);
-      var btn = document.getElementById("btn");
-      console.log(btn);
-      
-      var imgs = document.getElementById("container");
-      var title = document.getElementById("title");
-      var year = document.getElementById("year");
-      var awards = document.getElementById("awards");
-      var actors = document.getElementById("actors");
-      var director = document.getElementById("director");
-      var rating = document.getElementById("rating");
-      var boxoffice = document.getElementById("boxoffice");
-      var img = document.createElement("img");
-      
-      console.log(title, year, awards, actors, director, rating, boxoffice);
-     
-      async function getdata(name) {
-          try {
-              var fetchdata = await fetch(`https://www.omdbapi.com/?t=${name}&apikey=76d079f0`);
-              var data = await fetchdata.json();
-    
-              console.log(fetchdata);
-              console.log(data);
-              // console.log(data.Title);
-              
-              title.innerText = data.Title || " ";
-              year.innerText = data.Released || " ";
-              awards.innerText = data.Awards || " ";
-              actors.innerText = data.Actors || " ";
-              director.innerText = data.Director || " ";
-              rating.innerText = data.Ratings[0]?.Value || " ";
-              boxoffice.innerText = data.BoxOffice || "";
-              
-              img.src = data.Poster || " ";
-              imgs.appendChild(img);
-              
-              console.log(img);
-          } catch (err) {
-              console.log("Movie not found");
-          }
-      }
-  
-      btn.addEventListener("click", async () => {
-          var moviename = input.value;
-          console.log(moviename);
-          await getdata(moviename);
-    
-    // Show the container after fetching data
-    if (moviename) {
-        imgs.style.display = 'block'; // Show the container
+
+const API_URL = "http://localhost:3000/users";
+
+// Fetch users from JSON server
+async function fetchUsers() {
+    const response = await fetch(API_URL);
+    return await response.json();
+}
+
+// Register functionality
+const registerForm = document.getElementById("register-form");
+if (registerForm) {
+    registerForm.addEventListener("submit", (e) => {
+        e.preventDefault();
+
+        const username = document.getElementById("register-username").value;
+        const password = document.getElementById("register-password").value;
+
+        // Store in localStorage
+        let localUsers = JSON.parse(localStorage.getItem("users")) || [];
+        localUsers.push({ username, password });
+        localStorage.setItem("users", JSON.stringify(localUsers));
+
+        // Update JSON server
+        fetch(API_URL,{
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ username, password }),
+        }) .then((response) => {
+            // Check if the response is not OK (e.g., server error)
+            if (!response.ok) {
+                throw new Error("Failed to register user on the server");
+            }
+        })
+        .catch((error) => {
+            console.error(error);
+            alert("An error occurred during registration. Please try again.");
+        });
+
+    alert("User registered successfully! Redirecting to login page...");
+    window.location.href = "login.html";
+});
+}
+
+// Login functionality
+const loginForm = document.getElementById("login-form");
+if (loginForm) {
+loginForm.addEventListener("submit", async (e) => {
+    e.preventDefault();  const username = document.getElementById("login-username").value;
+    const password = document.getElementById("login-password").value;
+
+    // Check in localStorage
+    const localUsers = JSON.parse(localStorage.getItem("users")) || [];
+    const isLocalUser = localUsers.some(
+        (user) => user.username === username && user.password === password
+    );
+
+    // Check in JSON server
+    try {
+        const users = await fetchUsers();
+        const isServerUser = users.some(
+            (user) => user.username === username && user.password === password
+        );
+
+        if (isLocalUser || isServerUser) {
+            alert("Login successful! Redirecting to main page...");
+            window.location.href = "index.html"; // Create a index.html for the next page
+        } else {
+            alert("Invalid username or password!");
+        }
+    } catch (error) {
+        console.error(error);
+        // alert("An error occurred during login. Please try again.");
+        window.location.href = "index.html";
     }
-      });
-      
+});
+}
+"use strict";
+//starting the home page
+const header=document.querySelector("header");
+const menus=document.querySelector(".menus");
+const sliders=document.querySelectorAll(".slide");
+const LeftBtn=document.getElementById("Left");
+const SliderContentEL=document.querySelectorAll(".slider-content");
+const RightBtn=document.getElementById("right");
+
+window.addEventListener("scroll",()=>{
+    if(document.documentElement.scrollTop>20){
+        menus.classList.add("sticky")
+    }
+    else{
+        menus.classList.remove("sticky")
+    }
+})
+let activeSlide=3;
+//FOR setting the background of the body ,this function sets the background image of the body based on the backgroung image of sliders based on index
+function setBgBody(){
+    header.style.backgroundImage=sliders[activeSlide].style.backgroundImage;
+}
+setBgBody();
+
+function setActiveSlide(){
+    sliders.forEach((slides)=>slides.classList.remove("active"));//if we donot remove the active slide,already we have declared the active in index file for one slide ,now it mat get 2 or 3 slides at a time
+    sliders[activeSlide].classList.add("active");//adding active for the current slide and classlist is used for accessing methods like add,remove in  a easier way
+}
+//set content
+function setContent(){
+    SliderContentEL.forEach((slidersContents)=>{
+        slidersContents.classList.remove("active")
+    })
+    SliderContentEL[activeSlide].classList.add("active")
+    console.log(SliderContentEL)
+}
+RightBtn.addEventListener("click",()=>{
+    nextSlide()
+    setBgBody();
+    setActiveSlide();
+    setContent();
+});
+LeftBtn.addEventListener("click",()=>{
+    PreviousSlide()
+    setBgBody();
+    setActiveSlide();
+    setContent();
+});
+function nextSlide(){
+    activeSlide++;
+    if(activeSlide>sliders.length-1){
+        activeSlide=0;
+    }
+}
+function PreviousSlide(){
+    activeSlide--;
+    if(activeSlide<0){
+        activeSlide=sliders.length-1
+    }
+}
+/*setInterval(()=>{
+    nextSlide();
+    setBgBody();
+    setActiveSlide();
+    setContent();
+},1000)*/
